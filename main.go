11ccf573105682860fa1bfad02345194bd69bda8
Luís Nascimento <luisnascimento.comp@gmail.com>
2020-05-01 17:25:23 -0300

    apply diff skeletum in main routine

--- main.go
+++ main.go
@@ -18,6 +18,8 @@ import (
 )
 
 func main() {
+	ctx := context.Background()
+
 	oldPath := flag.String("old", "", "old file path")
 	newPath := flag.String("new", "", "new file path")
 	key := flag.String("key", "", "key column to use")
@@ -42,10 +44,31 @@ func main() {
 	}
 	defer db.Close()
 
-	eg, _ := errgroup.WithContext(context.Background())
+	eg, _ := errgroup.WithContext(ctx)
 
-	eg.Go(func() error { return createTable(db, *oldPath, *key) })
-	eg.Go(func() error { return createTable(db, *newPath, *key) })
+	var oldTblName, newTblName string
+	eg.Go(func() (err error) {
+		oldTblName, err = createTable(db, *oldPath, *key)
+		return err
+	})
+	eg.Go(func() (err error) {
+		newTblName, err = createTable(db, *newPath, *key)
+		return err
+	})
+
+	if err := eg.Wait(); err != nil {
+		log.Fatal(err)
+	}
+
+	inCh, outCh, err := diff(oldTblName, newTblName)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	eg, _ = errgroup.WithContext(ctx)
+
+	eg.Go(func() error { return proccessDiff("+", inCh) })
+	eg.Go(func() error { return proccessDiff("-", outCh) })
 
 	if err := eg.Wait(); err != nil {
 		log.Fatal(err)
@@ -58,16 +81,16 @@ func main() {
 	fmt.Println("Done")
 }
 
-func createTable(db *sql.DB, filepath, key string) error {
+func createTable(db *sql.DB, filepath, key string) (string, error) {
 	f, err := os.Open(filepath)
 	if err != nil {
-		return err
+		return "", err
 	}
 	defer f.Close()
 
 	stat, err := f.Stat()
 	if err != nil {
-		return err
+		return "", err
 	}
 
 	tblName := strings.Replace(stat.Name(), ".csv", "", 1)
@@ -75,17 +98,17 @@ func createTable(db *sql.DB, filepath, key string) error {
 
 	_, err = db.Exec(sql)
 	if err != nil {
-		return err
+		return "", err
 	}
 
 	tx, err := db.Begin()
 	if err != nil {
-		return err
+		return "", err
 	}
 
 	stmt, err := tx.Prepare(fmt.Sprintf("INSERT INTO %s(id, row) VALUES(?, ?)", tblName))
 	if err != nil {
-		return err
+		return "", err
 	}
 	defer stmt.Close()
 
@@ -93,12 +116,12 @@ func createTable(db *sql.DB, filepath, key string) error {
 
 	header, err := reader.Read()
 	if err != nil {
-		return err
+		return "", err
 	}
 
 	keyIdx, err := keyIndex(key, header)
 	if err != nil {
-		return err
+		return "", err
 	}
 
 	for {
@@ -108,20 +131,20 @@ func createTable(db *sql.DB, filepath, key string) error {
 		}
 
 		if err != nil {
-			return err
+			return "", err
 		}
 
 		_, err = stmt.Exec(row[keyIdx], strings.Join(row, ";"))
 		if err != nil {
-			return err
+			return "", err
 		}
 	}
 
 	if err := tx.Commit(); err != nil {
-		return err
+		return "", err
 	}
 
-	return nil
+	return tblName, nil
 }
 
 func keyIndex(key string, header []string) (int, error) {
@@ -135,11 +158,19 @@ func keyIndex(key string, header []string) (int, error) {
 }
 
 func diff(oldTblName, newTblName string) (chan []string, chan []string, error) {
-	oldCh := make(chan []string)
-	newCh := make(chan []string)
+	inCh := make(chan []string)
+	outCh := make(chan []string)
 
-	close(oldCh)
-	close(newCh)
+	close(inCh)
+	close(outCh)
 
-	return oldCh, newCh, nil
+	return inCh, outCh, nil
+}
+
+func proccessDiff(diffType string, rowsCh chan []string) error {
+	for row := range rowsCh {
+		fmt.Printf("[%s] %s\n", diffType, row)
+	}
+
+	return nil
 }
